c880ab1a2758a17e296ee722178603222a0e9759
Sergey Kovyazin <mr.roland1989@gmail.com>
2024-04-03 06:13:10 +0300

    todo-app

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,8 @@ import { Provider } from 'react-redux';
 import { App } from './App';
 import { store } from './app/store';
 
-createRoot(document.getElementById('root') as HTMLDivElement)
-  .render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  );
+createRoot(document.getElementById('root') as HTMLDivElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
